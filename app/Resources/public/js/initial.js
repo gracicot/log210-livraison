96d9e3eff864bc818be427f2e075728da4ada9ca
Guillaume Racicot <gufideg@gmail.com>
2015-05-22 04:04:38 -0400

    ajout des traductions et tooltip bootstrap

--- app/Resources/public/js/initial.js
+++ app/Resources/public/js/initial.js
@@ -7,5 +7,11 @@
 
     	element('[data-href]').linkify();
     	element('[data-toggle="tooltip"]').tooltip();
+    	element('textarea').each(function () {
+			this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+		}).on('input', function () {
+			this.style.height = 'auto';
+			this.style.height = (this.scrollHeight) + 'px';
+		});
     };
 }( jQuery ));
