8af5152a687c52cbefcc7ecac29f6ad556495215
MAM <ma.majeau@gmail.com>
2015-05-27 22:29:57 -0400

    Ajout des entités menu et plats, des vues et controleurs

---
+++ src/Log210/LivraisonBundle/Controller/PlatController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Log210\LivraisonBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Log210\CommonBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Log210\LivraisonBundle\Entity\Plat;
+use Log210\LivraisonBundle\Form\PlatType;
+
+/**
+ * Plat controller.
+ *
+ * @Route("/plat")
+ */
+class PlatController extends Controller
+{
+
+    protected function getRoutes()
+    {
+        return [
+            'show' => 'plat_show',
+            'new' => 'plat_new',
+            'update' => 'plat_update',
+            'delete' => 'plat_delete',
+            'create' => 'plat_create',
+            'edit' => 'plat_edit'
+        ];
+    }
+    protected function getRepository()
+    {
+        return $this->getDoctrine()->getRepository('Log210LivraisonBundle:Plat');
+    }
+
+    protected function getForm()
+    {
+        return new PlatType();
+    }
+
+    /**
+     * Lists all Plat entities.
+     *
+     * @Route("/", name="plat")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+        return parent::indexAction($request);
+    }
+    /**
+     * Creates a new Plat entity.
+     *
+     * @Route("/", name="plat_create")
+     * @Method("POST")
+     * @Template("Log210LivraisonBundle:Plat:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        return parent::createAction($request);
+    }
+
+    /**
+     * Displays a form to create a new Plat entity.
+     *
+     * @Route("/new", name="plat_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction(Request $request)
+    {
+        return parent::newAction($request);
+    }
+
+    /**
+     * Finds and displays a Plat entity.
+     *
+     * @Route("/{id}", name="plat_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction(Request $request, $id)
+    {
+        return parent::showAction($request, $id);
+    }
+
+    /**
+     * Displays a form to edit an existing Plat entity.
+     *
+     * @Route("/{id}/edit", name="plat_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction(Request $request, $id)
+    {
+        return parent::editAction($request, $id);
+    }
+
+    /**
+     * Edits an existing Plat entity.
+     *
+     * @Route("/{id}", name="plat_update")
+     * @Method("PUT")
+     * @Template("Log210LivraisonBundle:Plat:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        return parent::updateAction($request, $id);
+    }
+    /**
+     * Deletes a Plat entity.
+     *
+     * @Route("/{id}", name="plat_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        return parent::deleteAction($request, $id);
+    }
+
+}
---
+++ src/Log210/LivraisonBundle/Form/MenuType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Log210\LivraisonBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MenuType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            /*->add('restaurant')*/
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Log210\LivraisonBundle\Entity\Menu'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'log210_livraisonbundle_menu';
+    }
+}
