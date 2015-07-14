c87a5c7b3dffaeea8853e746a529d5fafab37116
Tomasz Hamryszak <tomasz.hamryszak.1@ens.etsmtl.ca>
2015-07-14 11:27:26 -0400

    Fonctionnel

--- src/Log210/LivraisonBundle/Entity/Client.php
+++ src/Log210/LivraisonBundle/Entity/Client.php
@@ -9,55 +9,12 @@ use Log210\UserBundle\Entity\User;
  * @ORM\Entity
  * @ORM\Table(name="clients")
  */
-class Client
-{
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
+class Client extends User {
 
     /**
      * @ORM\Column(type="text")
      */
     protected $address;
-
-    /**
-     * @ORM\OneToOne(targetEntity="\Log210\UserBundle\Entity\User", inversedBy="client")
-     * @ORM\JoinColumn(name="client_id", referencedColumnName="id", nullable=true)
-     */
-    protected $user;
-
-    /**
-     * Set restaurants
-     *
-     * @param \Log210\LivraisonBundle\Entity\Restaurant $restaurants
-     * @return Restaurateur
-     */
-    public function setUser(User $user = null)
-    {
-        $oldUser = $this->user;
-        $this->user = $user;
-
-        if ($oldUser !== null && $oldUser->getClient() === $this) {
-            $oldUser->setClient(null);
-        }
-
-        if ($user !== null && $user->getClient() !== $this) {
-            $user->setClient($this);
-        }
-    }
-
-    /**
-     * Get restaurant
-     *
-     * @return \Log210\LivraisonBundle\Entity\Restaurant
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
 
     public function getAddress()
     {
@@ -69,26 +26,5 @@ class Client
         $this->address = $address;
 
         return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function __toString()
-    {
-        return (string)$this->getUser();
     }
 }
